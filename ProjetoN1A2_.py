e6e63668df2dc22fa554de3332f07d8945eeb6cf
Rubens J. Souza <90809866+SneburAzuos@users.noreply.github.com>
2021-10-15 20:55:42 -0300

    Novo projeto

---
+++ ProjetoN1A2_.py
@@ -0,0 +1,72 @@
+listaUsuarios = []
+
+
+def alterarNome():
+    print("fon")
+    main()
+def removerUsuario():
+    print("fon")
+    main()
+    
+def verificarNome(listaUsuarios):
+    names = input("Digite um nome para pesquisar na lista: ")
+    if(listaUsuarios.cont(names)):
+       print(names in listaUsuarios)
+    main()
+
+def listarAlfabetica(listaUsuarios):
+    print("fon")
+    main()
+
+def listarCadastro(listaUsuarios):
+    print("Lista por ordem de cadastro: ")
+    num = 1
+    for cadastro in listaUsuarios:
+        print("{0} - Nome: {1}, Email: {2}".format(num, cadastro["nome"], cadastro["email"]))
+        num = num + 1
+    main()
+
+def cadastrarUsuario():
+    usuarios = {}
+    usuarios["nome"] = input("Digite o nome completo do usuário: ")
+    usuarios["email"] = input("Digite o email do usuário: ")
+    return usuarios
+        
+
+def mostrarMenu():
+    print("----------------------------------Menu----------------------------------")
+    print("1 - Cadastrar novo usuário")
+    print("2 - Listar usuários por ordem de cadastro")
+    print("3 - Listar usuários por ordem alfabética")
+    print("4 - Verificar se um usuário faz parte da lista através do nome")
+    print("5 - Remover usuário")
+    print("6 - Alterar nome de usuário")
+    print("7 - Fechar programa")
+        
+def main():
+    mostrarMenu()
+
+    option = int(input("Escolha uma opção: "))
+    while(option < 1 or option > 7):
+        print("Você escolheu uma opção inválida! Tente novamente.")
+        option = int(input("Escolha uma opção: "))
+
+    if(option == 1):
+        usuarios = cadastrarUsuario()
+        listaUsuarios.append(usuarios)
+        main()
+    elif(option == 2):
+       listarCadastro(listaUsuarios)
+    elif(option == 3):
+        listarAlfabetica(listaUsuarios)
+    elif(option == 4):
+        verificarNome(listaUsuarios)
+    elif(option == 5):
+        removerUsuario()
+    elif(option == 6):
+        alterarNome()
+    else:
+        breakpoint
+
+if __name__ == "__main__":
+    main()
